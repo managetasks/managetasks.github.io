0f6bc1b02bf87d3eb9a583c7d4affc7640d56085
Trev17 <tjpastega@gmail.com>
2015-06-26 12:05:34 -0700

    STuF

---
+++ app.js
@@ -0,0 +1,8 @@
+var main = function() {
+    $('.btn').click(function() {
+        var post = $('.status-box').val();
+        $('<li>').text(post).prependTo('.posts');
+        $('.status-box').val('');
+    });
+
+$(document).ready(main);
